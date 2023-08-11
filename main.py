8a65f0fa3d4476ddd053589621fada97c7059d95
Brenden Moore <brendenm3603@gmail.com>
2023-08-11 00:11:48 -0700

    astroid to appear at random x axis, added stars to appear like traveling through space

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame
 import os
+import random
 
 # Define constants in caps sense they wont change
 
-WINDOW_WIDTH, WINDOW_HEIGHT = 900, 500  # These variables define our "window", width, height
+WINDOW_WIDTH, WINDOW_HEIGHT = 1000, 700  # These variables define our "window", width, height
 BORDER_WIDTH, BORDER_COLOR = 8, (255,255,255)
 WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Game")  # Window caption
@@ -19,6 +20,33 @@ def BORDER():  # Border for my game
     pygame.draw.rect(WINDOW, BORDER_COLOR, (WINDOW_WIDTH - BORDER_WIDTH, 0, BORDER_WIDTH, WINDOW_HEIGHT))  # Right border
 
 
+# Potential start screen section
+def START_SCREEN():
+    WINDOW.fill(BLACK)
+
+
+
+stars = []
+
+def create_stars(surface, num_stars):
+    global stars
+
+    # Remove stars that have gone off the screen
+    stars = [(x, y + 1) for x, y in stars if y < WINDOW_HEIGHT]
+
+    # Add new stars
+    for _ in range(num_stars - len(stars)):
+        x = random.randint(0, WINDOW_WIDTH)
+        y = random.randint(-WINDOW_HEIGHT, 0)
+        stars.append((x, y))
+
+    # Draw stars
+    for x, y in stars:
+        pygame.draw.circle(surface, (255, 255, 255), (x, y), 1)
+
+
+
+# Spaceship
 SPACE_WIDTH, SPACE_HEIGHT = 70, 90
 SPACESHIP_IMAGE = pygame.image.load(
     os.path.join('assets', 'spaceship.png'))
@@ -28,10 +56,28 @@ SPACESHIP = pygame.transform.scale(SPACESHIP_IMAGE, (SPACE_WIDTH, SPACE_HEIGHT))
 ship = pygame.Rect(420, 350, SPACE_WIDTH, SPACE_HEIGHT)
 
 
+# Astroids
+ASTROID_WIDTH, ASTROID_HEIGHT = 70, 90
+ASTROID_IMAGE = pygame.image.load(
+    os.path.join('assets', 'astroid.png'))
+
+ASTROID_ROTATE_SPEED = 2
+
+ASTROID = pygame.transform.scale(ASTROID_IMAGE, (ASTROID_WIDTH, ASTROID_HEIGHT))
+
+astroid_x = random.randint(0, WINDOW_WIDTH - ASTROID_WIDTH)
+astroid = pygame.Rect(astroid_x, 0,  ASTROID_WIDTH, ASTROID_HEIGHT)
+
+
+
+
 def draw_window(ship):
     WINDOW.fill(BLACK)  # Draw background first
+    create_stars(WINDOW, 200)  # Creates stars
     BORDER()
+
     WINDOW.blit(SPACESHIP, (ship.x, ship.y))  # Draws spaceship start position
+    WINDOW.blit(ASTROID, (astroid.x, 0))  # Draw the asteroid
 
     pygame.display.update()  # Updating our window
 
@@ -55,6 +101,7 @@ def main():
     
     clock = pygame.time.Clock()
     run = True
+
 
     while run:
         clock.tick(FPS)  # frame refresh rate
